0e6a720f71cd7e812b1577c82592041dd69c2f20
JuanDavidAvilaRaveloCampus <juandavidavilaravelo856@gmail.com>
2023-08-03 15:13:47 -0500

    feat: :sparkles: Sirve, pero no hay error personalizado
    la consulta por tamaño como respuesta en el body, sirve, sin embargo la respuesta de error no es personalizable

--- backend/src/routers/campus.js
+++ backend/src/routers/campus.js
@@ -4,20 +4,30 @@ import express from "express";
 
 const appCampus = Router();
 
-const tamaño = express.json({limit : "100B"})
+const tamaño = express.json({ limit: '100KB' })
 
-appCampus.get("/", confiGET(), (req,res)=>{
+appCampus.get("/", confiGET(), (req, res) => {
     console.log(req.rateLimit);
     res.send("Hola");
 });
 
-appCampus.post("/campus", confiGET(), tamaño, (req,res)=>{
+appCampus.post("/campus", confiGET(), tamaño, (req, res, err) => {
+    // solicitud en el body
     const jsonBody = req.body;
     const json = JSON.stringify(jsonBody);
-    let tamaño = Buffer.byteLength(json, 'utf8');
+    let tamaño_bytes = Buffer.byteLength(json, 'utf8');
+
+    // tamaño en bytes de la solicitud
     res.jsonBody;
-    console.log('Tamaño de la solicitud: ', tamaño, ' bytes');
     res.status(200).send('Solicitud recibida correctamente');
+    console.log('Tamaño de la solicitud: ', tamaño_bytes, ' bytes');
+    
+    //! ESTE ES UN EJEMPLO DE COMO SE ENVIA REALMENTE UN JSON Y EL VALOR REAL EN BYTES QUE TIENE
+    // const ejemplo = `{    'nombre':'Juan',    'edad':30,    'email':'juan@example.com',    'telefono':'12345678900000'}`
+    // console.log(
+    //     `${ejemplo}: ${ejemplo.length} characters, ` +
+    //     `${Buffer.byteLength(ejemplo, 'utf8')} bytes`
+    // );
 });
 
 export default appCampus;
